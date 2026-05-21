54356b16584a91fad827fe0e661ff1cd981d9afc
eugeniodias5 <eugeniodias5@hotmail.com>
2026-05-21 09:47:30 +0200

    Add MLFlow tracking of models' parameters and performance metrics

--- train/train.py
+++ train/train.py
@@ -2,7 +2,17 @@ from pathlib import Path
 
 from loan_approval_predictor.train import train_model
 
+import mlflow
+
+
 if __name__ == "__main__":
-    data_path = Path(__file__).parent.parent / "datasets" / "loan_approval_dataset.csv"
-    train_model(data_path)
+    mlflow.set_tracking_uri("http://localhost:5000")
+    exp_id = mlflow.create_experiment("loan_approval_predictor")
+    mlflow.set_experiment("loan_approval_predictor")
+
+    with mlflow.start_run(run_name="loan-v1"):
+        mlflow.set_tag("model_version")
+        data_path = Path(__file__).parent.parent / "datasets" / "loan_approval_dataset.csv"
+        train_model(data_path)
+        
     
